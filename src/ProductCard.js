a30eefdd464cd0859f4de9dddc350fe9babc628b
wise-toddler <wiseeldrich2004@gmail.com>
2024-07-03 11:53:58 +0530

    learned to use hook useRef()

--- src/ProductCard.js
+++ src/ProductCard.js
@@ -1,9 +1,18 @@
-function ProductCard({title,price}){
+import { useRef } from 'react';
+import './ProductCard.css';
+function ProductCard({ title, price }) {
+    let pRef = useRef();
+    const printPrince = () => {
+        if (pRef.current.style.display === "none")
+            pRef.current.style.display = "block";
+        else
+            pRef.current.style.display = "none";
+    }
     return (
-        <div>
+        <div className="product-card">
             <h2>Product Card</h2>
-            <h3>{title}</h3>
-            <p>{price}</p>
+            <h3 onClick={printPrince}>{title}</h3>
+            <p ref={pRef}>{price}</p>
         </div>
     )
 }
